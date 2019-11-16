61b5d47d8c168903d347e1ac5dd7804326616a8b
Michael Haberler <git@mah.priv.at>
2019-11-16 14:58:21 +0100

    config.js: use rikitrakiws mah backend

--- js/config.js
+++ js/config.js
@@ -2,8 +2,8 @@
 
 export var tmConfig = {
   	getApiBaseUrl: function () {
-        return 'http://localhost:3000/api';
-		//return 'https://rikitrakiws.mah.priv.at/api';
+        	//return 'http://localhost:3000/api';
+		return 'https://rikitrakiws.mah.priv.at/api';
 		// return 'https://services.rikitraki.com/api';
   	},
   	getTrackId: function () {
